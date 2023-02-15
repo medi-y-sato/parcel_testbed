1a0ff555edd9d06b57b001f82f887f2eaa83bd0d
[mediba] Yoshiaki Satoh <y-sato@mediba.jp>
2023-02-15 15:08:10 +0900

    テストをテストする

--- src/ts/logger.ts
+++ src/ts/logger.ts
@@ -1,5 +1,9 @@
 export default class logger {
   log(message: string): void {
-    console.log(`logger : ${message}`);
+    console.log(this.messageAddApendix(message));
+  }
+
+  messageAddApendix(str: string): string {
+    return `logger : ${str}`;
   }
 }
